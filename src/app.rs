5819b5a7ee2583fce588d05d6d3395400a2a7306
maxroyer <maxroyer86@gmail.com>
2021-11-14 10:57:12 -0600

    New button

--- src/app.rs
+++ src/app.rs
@@ -3,9 +3,11 @@ use super::file::*;
 pub struct CodeShare {
     file_status: FileStatus,
     text_buf: String,
+    is_buffer_saved: bool,
     is_open_window: bool,
     is_save_window: bool,
     is_save_as_window:bool,
+    is_unsaved_window: bool,
     file_to_open: String,
 }
 
@@ -13,10 +15,12 @@ impl Default for CodeShare {
     fn default() -> Self {
         Self {
             file_status: FileStatus::default(),
-            text_buf: "Hello World!".to_owned(),
+            text_buf: String::new(),
+            is_buffer_saved: false,
             is_open_window: false,
             is_save_window: false,
             is_save_as_window: false,
+            is_unsaved_window: false,
             file_to_open: String::from(std::env::var("HOME").unwrap()),
         }
     }
@@ -34,12 +38,14 @@ impl epi::App for CodeShare {
         _frame: &mut epi::Frame<'_>,
         _storage: Option<&dyn epi::Storage>,
     ) {
+        /*
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         #[cfg(feature = "persistence")]
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
+        */
     }
 
     /// Called by the frame work to save state before shutdown.
@@ -52,12 +58,27 @@ impl epi::App for CodeShare {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        let Self { file_status, text_buf, is_open_window, is_save_window, is_save_as_window, file_to_open, } = self;
+        let Self {
+            file_status,
+            text_buf, is_buffer_saved,
+            is_open_window, is_save_window,
+            is_save_as_window,
+            is_unsaved_window,
+            file_to_open,
+        } = self;
 
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("New").clicked() {
+                        match *is_buffer_saved {
+                            true => text_buf.clear(),
+                            false => {
+                                *is_unsaved_window = true;
+                            }
+                        }
+                    }
                     if ui.button("Open").clicked(){
                         *is_open_window = true;
                     }
@@ -89,6 +110,7 @@ impl epi::App for CodeShare {
                             Ok(c) => c,
                             Err(e) => panic!("Error: {}",e)
                         };
+                        *is_buffer_saved = true;
                         *is_open_window = false;
                     }
                     if ui.button("Close").clicked() {
@@ -97,13 +119,14 @@ impl epi::App for CodeShare {
                 });
             });
         }
-
+        // File save status window
         if *is_save_window {
             egui::Window::new("Save Status").show(ctx, |ui| {
                 match file_status.save_file(text_buf) {
                     Ok(_) => {
                         ui.label("Save Successful");
                         if ui.button("OK").clicked() {
+                            *is_buffer_saved = true;
                             *is_save_window = false;
                             *is_save_as_window = false;
                         }
@@ -113,13 +136,12 @@ impl epi::App for CodeShare {
                         ui.label(error);
                         if ui.button("OK").clicked() {
                             *is_save_window = false;
-                            *is_save_as_window = true;
                         }
                     }
                 };
             });
         }
-
+        //   Save as window
         if *is_save_as_window {
             egui::Window::new("Save As").show(ctx, |ui| {
                 ui.text_edit_singleline(file_to_open);
@@ -129,8 +151,28 @@ impl epi::App for CodeShare {
                         *is_save_window = true;
                         
                     }
-                    if ui.button("Quit").clicked() {
+                    if ui.button("Close").clicked() {
                         *is_save_as_window = false;
+                    }   
+                });
+            });
+        }
+        //  File not saved popup
+        if *is_unsaved_window {
+            egui::Window::new("File Not Saved").show(ctx, |ui| {
+                ui.label("Current file has not been saved");
+                ui.horizontal( |ui| {
+                    if ui.button("Save").clicked() {
+                        *is_save_window = true;
+                        *is_unsaved_window = false;
+                    }
+                    if ui.button("Save As").clicked() {
+                        *is_save_as_window = true;
+                        *is_unsaved_window = false;
+                    }
+                    if ui.button("Continue without saving").clicked() {
+                        *is_unsaved_window = false;
+                        text_buf.clear();
                     }   
                 });
             });
@@ -138,13 +180,24 @@ impl epi::App for CodeShare {
 
         egui::CentralPanel::default().frame(egui::Frame::none().corner_radius(0.0)).show(ctx, |ui| {
             ScrollArea::vertical().show(ui, |ui|{
-                ui.add_sized(ui.available_size(),
-                    egui::TextEdit::multiline(text_buf)
-                        .text_style(egui::TextStyle::Monospace)
-                        .code_editor()
-                        .lock_focus(true)
-                        .desired_width(f32::INFINITY)
-                )
+                let editor = ui.add_sized(ui.available_size(),
+                egui::TextEdit::multiline(text_buf)
+                    .text_style(egui::TextStyle::Monospace)
+                    .code_editor()
+                    .lock_focus(true)
+                    .desired_width(f32::INFINITY)
+                );
+                if editor.changed() {
+                    *is_buffer_saved = false;
+                }
+
+                // ui.add_sized(ui.available_size(),
+                //     egui::TextEdit::multiline(text_buf)
+                //         .text_style(egui::TextStyle::Monospace)
+                //         .code_editor()
+                //         .lock_focus(true)
+                //         .desired_width(f32::INFINITY)
+                // )
             });
         });
 
